2c23641599b1ad87a6a08daee5960975e5720bed
Sleeping <hh.sleeping@gmail.com>
2016-09-20 18:09:53 -0400

    Revert "add network script, FAIL"
    This reverts commit 85660b4811f1d1090b05743898772f935ed76ad9.

--- xmobar.hs
+++ xmobar.hs
@@ -6,7 +6,10 @@ Config {
     position = Static { xpos = 0, ypos = 0, width = 1366, height = 16 },
     lowerOnStart = True,
     commands = [
-        ,Run Date "<fc=#b0d2ff>%a %b %d </fc>%H:%M" "date" 600 
+         Run DynNetwork [
+             "-t"    ,"<dev>"
+            ] 10
+        ,Run Date "<fc=#b0d2ff>%a %b %d </fc>%H:%M" "date" 10 
 		,Run MultiCpu [ "--template" , "<autototal>"
             , "--Low"      , "50"         -- units: %
             , "--High"     , "85"         -- units: %
@@ -17,12 +20,11 @@ Config {
             , "-w"         , "3"
         ] 10
         ,Run Com "bash" ["-c", ".xmonad/getvolume.sh"] "vol" 10
-        ,Run Com "bash" ["-c", ".xmonad/network.sh"] "network" 10 
         ,Run Com "bash" ["-c", ".xmonad/battery.sh"] "battery" 10
         ,Run Kbd [("ru", "ru"), ("us", "en")]
         ,Run StdinReader
     ],
     sepChar = "%",
     alignSep = "}{",
-    template = " %StdinReader% }{ <fc=#b0d2ff> %kbd%</fc> <fc=#f6e8b9><icon=/home/sleeping/.xmonad/icons/spkr_01.xbm/></fc> %vol% <fc=#74dea7><icon=/home/sleeping/.xmonad/icons/cpu.xbm/></fc>%multicpu%  %network% <icon=/home/sleeping/.xmonad/icons/bat_low_01.xbm/> %battery%  %date% "
+    template = " %StdinReader% }{<fc=#b0d2ff> %kbd%</fc> <fc=#f6e8b9><icon=/home/sleeping/.xmonad/icons/spkr_01.xbm/></fc> %vol% <fc=#74dea7><icon=/home/sleeping/.xmonad/icons/cpu.xbm/></fc>%multicpu% <fc=#aa77dd> <icon=/home/sleeping/.xmonad/icons/wifi_02.xbm/></fc> (%dynnetwork%) <icon=/home/sleeping/.xmonad/icons/bat_low_01.xbm/> %battery%  %date% "
 }
